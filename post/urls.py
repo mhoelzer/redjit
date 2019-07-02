fc88e1d78f7b87de86a4bf37cf06bd0334f4c1d5
Jennifer Browning <browningjen@yahoo.com>
2019-07-02 10:38:25 -0400

    post directory

---
+++ post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from redjit.post.views import MyPost, PostView
+
+
+
+urlpatterns = [
+    path('newpost/', MyPost.as_view(), name='newpost')
+    path('subredjit/<subredjit>/<post_id>/', PostView.as_view(), name='post')
+]
